91d80a449a670284014708b1a7be2cf0ac7c6833
Yuri Ferretti <yuriferretti@gmail.com>
2017-06-29 11:27:22 -0300

    Slides

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -3,12 +3,47 @@
 import UIKit
 import RxSwift
 
-private let disposeBag = DisposeBag()
+enum E: Error {
+    case foo
+}
 
-Observable.just("2")
-    .subscribe(onNext: { value in
-        print(value)
+let publishSubject = ReplaySubject<String>.create(bufferSize: 2)
+publishSubject.onNext("1")
+publishSubject.onNext("2")
+publishSubject.onNext("3")
+
+var variable: Variable? = Variable<String>("initial")
+
+//variable?.asObservable()
+//    .debug("publish", trimOutput: true)
+//    .subscribe(onNext: { (value) in
+//    print(value)
+//}, onError: { (error) in
+//    print("Error")
+//}, onCompleted: { 
+//    print("completed")
+//})
+
+variable?.value = "second"
+variable?.value = "third"
+variable = nil
+
+publishSubject.onCompleted()
+publishSubject.onNext("4")
+
+struct S {
+    let a: Int
+}
+
+Observable.from([S(a: 1), S(a:1)])
+    .distinctUntilChanged({ (old, new) -> Bool in
+        old.a == new.a
     })
-    .disposed(by: disposeBag)
+    .subscribe { (value) in
+        print(value)
+    }
 
 playgroundTimeLimit(seconds: 10)
+
+
+
